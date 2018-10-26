6bfdf57d429f61a6f035f221cf86d4769be95a57
huanjunzi <hjz@epeijing.cn>
2018-10-26 15:27:26 +0800

    增加table的filter筛选

--- src/custom/index.vue
+++ src/custom/index.vue
@@ -11,6 +11,8 @@
     import {showModal} from '../modals'
     import tableEdit from './tableEdit'
     import _ from 'underscore'
+    import {formatDate} from '../utils/timeFormat'
+
     export default {
         data () {
             return {
@@ -23,6 +25,7 @@
                 selectedItems: [],
                 // 批量按钮隐藏和显示
                 showButton: false,
+                filterValue: {},
                 historyColumns: [
                     {
                         type: 'selection',
@@ -33,6 +36,7 @@
                         title: '名字',
                         key: 'name',
                         // className: 'demo-table-info-column'
+                        sortable: true,
                     },
                     {
                         title: '描述',
@@ -40,11 +44,37 @@
                     },
                     {
                         title: '原价',
-                        key: 'origin_price'
+                        key: 'origin_price',
+                        sortable: true
                     },
                     {
                         title: '现价',
-                        key: 'price'
+                        key: 'price',
+                         filters: [
+                            {
+                            label: '现价',
+                            value: '2'
+                            },
+                            {
+                            label: '原价',
+                            value: '36.8'
+                            }
+                        ],
+                        filterRemote: function (value,row) {
+                            // this.status = value;
+                            this.filterValue["price"] = value
+                            this.handleListApproveHistory()
+                            
+                            // console.log('filterValue', this.filterValue)
+                        }
+                    },
+                    {
+                        title: '创建时间',
+                        key: 'create_time',
+                        sortable: true,
+                        render: (h, ctx) => {
+                            return <div>{formatDate(ctx.row.create_time)}</div>
+                        }
                     },
                     {
                         title: '库存',
@@ -54,6 +84,16 @@
                                 return <div>没有库存</div>
                             }
                             return <div>{ctx.row.stock}</div>             
+                        },
+                        filters: [{
+                            label: "有库存",
+                            value: 5 
+                        },{
+                            label: "没库存",
+                            value: 0
+                        }],
+                        filterMethod(value, row) {
+                            return row.stock === value
                         }
                     },
                     {
@@ -74,52 +114,68 @@
             }
         },
         created(){
-            this.handleListApproveHistory();
+            this.handleListApproveHistory()
         },
         methods:{
-            // 获取历史记录信息
-            async handleListApproveHistory(){
+            async pageSend(type,url,limit,offset,filterValue = {}){
                 let r = await this.$axios({
-                methods: 'get',
-                url: '/api/users/pageQuery',
-                params: {
-                    limit: 0,
-                    offset: 0
-                }
+                    methods: type,
+                    url: url,
+                    params: {
+                        limit,
+                        offset,
+                        filter: filterValue
+                    }
                 }).then(res => res.data)
+                // console.log('r==', r)
+                return r
+            },
+            // 禁用复选框
+            disabledRow(data){
+                for (var i = 0; i < data.length; i++) {
+                 if (+data[i].stock >= 5) {
+                    data[i]._disabled = true // 设置复选框禁用
+                    data[i]._checked= true // 设置复选框选中状态
+                 }
+                }
+            },
+            // 获取历史记录信息
+            
+            async handleListApproveHistory(){
+                console.log('this.filterValue.price', this.filterValue.price)
+                if(_.isEmpty(this.filterValue.price)) {
+                    this.filterValue = {}
+                }
+                let r = await this.pageSend('get','/api/users/pageQuery',0,0,this.filterValue)
                 if(r.rows.length < this.pageSize){
                     this.historyData = r.rows
+                    this.dataCount = r.rows.length
                     return
                 } 
                 this.dataCount = r.rows.length
                 this.historyData = r.rows.slice(0, this.pageSize)
+                this.disabledRow(this.historyData)
             },
             async changepage(index){
                 var _start = ( index - 1 ) * this.pageSize;
                 var _end = this.pageSize;
-                let r = await this.$axios({
-                methods: 'get',
-                url: '/api/users/pageQuery',
-                params: {
-                  limit : _start,
-                  offset : _end
-                }
-                }).then(res => res.data)
+                let r = await this.pageSend('get', '/api/users/pageQuery', _start, _end, this.filterValue)
                 this.historyData = r.rows;
+                if(!_.isEmpty(this.filterValue.price)) {
+                   this.dataCount = r.rows.length
+                }
+                this.disabledRow(this.historyData)
             },
             async handlePageSize(index){
               this.pageSize = index
               let _start = (this.pageNum  - 1) * index;
               let _end = this.pageNum  * index;
-              let r = await this.$axios({
-                methods: 'get',
-                url: '/api/users/pageQuery',
-                params: {
-                  limit : _start,
-                  offset : _end
-                }
-                }).then(res => res.data)
+                let r = await this.pageSend('get', '/api/users/pageQuery', _start, _end, this.filterValue)
                 this.historyData = r.rows;
+                if(!_.isEmpty(this.filterValue.price)) {
+                   this.dataCount = r.rows.length
+                }
+                this.disabledRow(this.historyData)
             },
             selectChange(selection) {
             this.selectedItems = []
